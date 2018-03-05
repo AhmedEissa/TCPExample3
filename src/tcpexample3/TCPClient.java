7021464f9416e2f0587f28064e89f3e529c856e8
Matthew <matbulat@gmail.com>
2018-03-05 12:25:38 +0000

    updated client

--- src/tcpexample3/TCPClient.java
+++ src/tcpexample3/TCPClient.java
@@ -2,32 +2,36 @@ package tcpexample3;
 
 import java.io.*;
 import java.net.*;
+
 /**
  *
  * @author Matthew
  */
 class TCPClient {
+
     static TextProtocol textProt;
+
     public static void main(String argv[]) throws IOException, ClassNotFoundException {
         System.out.println("Client ready!");
 
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         boolean endLoop = true;
         Socket clientSocket = new Socket("localhost", 6789);
+        ObjectOutputStream outStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream inStream = new ObjectInputStream(clientSocket.getInputStream());
+
         while (endLoop) {
-            ObjectOutputStream outStream = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream inStream = new ObjectInputStream(clientSocket.getInputStream());
-            
+
             System.out.println("\nEnter instruction for server: \n"
                     + "saveMessage to save message to a file \n"
                     + "readFile to read file from server \n"
                     + "closeConnection to terminate connection with server.");
             String action = inFromUser.readLine();
-            switch(action){
+            switch (action) {
                 case "saveMessage":
                     System.out.println("Enter message: ");
                     String message = inFromUser.readLine();
-                    textProt = new TextProtocol(action,message);
+                    textProt = new TextProtocol(action, message);
                     outStream.writeObject(textProt);
                     textProt = (TextProtocol) inStream.readObject();
                     System.out.println(textProt.getHeader());
@@ -36,14 +40,14 @@ class TCPClient {
                     textProt = new TextProtocol(action);
                     outStream.writeObject(textProt);
                     textProt = (TextProtocol) inStream.readObject();
-                    for(String line : textProt.getStringArray()){
+                    for (String line : textProt.getStringArray()) {
                         System.out.println(line);
                     }
                     break;
                 case "closeConnection":
                     textProt = new TextProtocol(action);
                     outStream.writeObject(textProt);
-                    endLoop=false;
+                    endLoop = false;
                     break;
             }
         }
